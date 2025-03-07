fc00b21c11ec13fc71af84e42b956b400041ed5f
Ananthu M <ananthu.m.utr@gmail.com>
2025-03-07 10:07:01 +0530

    selection sort update

--- src/main/java/week00/SelectionSort.java
+++ src/main/java/week00/SelectionSort.java
@@ -1,7 +1,32 @@
 package week00;
 
 public class SelectionSort {
+    public static void selectionSort(int[] nums){
+        int min,temp;
+        for (int i = 0;i < nums.length; i++){
+            min = i;
+            for (int j = i+1;j < nums.length;j++){
+                if(nums[j] < nums[min]){
+                    min = j;
+                }
+            }
+            if(min != i){
+                temp = nums[min];
+                nums[min] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
     public static void main(String[] args) {
-
+        int[] nums = {3,2,4,1,7,5,8,9,6,10};
+        System.out.println("array before sorting");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\narray after sorting");
+        selectionSort(nums);
+        for (int num : nums){
+            System.out.print(num+" ");
+        }
     }
 }
