63a45b324a559f21259b93ae04c9acbdc1adb8dc
Ananthu M <ananthu.m.utr@gmail.com>
2025-03-07 10:26:56 +0530

    insertion sort

---
+++ src/main/java/week00/InsertionSort.java
@@ -0,0 +1,28 @@
+package week00;
+
+public class InsertionSort {
+    public static void insertionSort(int[] nums){
+        int key,temp;
+        for(int i = 0;i < nums.length;i++){
+            key = nums[i];
+            int j = i-1;
+            while (j >= 0 && nums[j] > key){
+                nums[j+1] = nums[j];
+                j--;
+            }
+            nums[j+1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,3,1,5,2,9,6,10,7,8};
+        System.out.println("array before sorting");
+        for (int num : nums){
+            System.out.print(num+" ");
+        }
+        System.out.println("\narray after sorting");
+        insertionSort(nums);
+        for (int num : nums){
+            System.out.print(num+" ");
+        }
+    }
+}
