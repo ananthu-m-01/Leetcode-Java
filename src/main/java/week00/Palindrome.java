2c5e4c9ba63e28d789f73ca336b3dddc76eca49f
Ananthu M <ananthu.m.utr@gmail.com>
2025-03-07 15:46:12 +0530

    palindrome

---
+++ src/main/java/week00/Palindrome.java
@@ -0,0 +1,21 @@
+package week00;
+
+public class Palindrome {
+    public static boolean isPalindrome(String str){
+        for(int i = 0, j = str.length()-1; i <str.length() && j>=0; i++,j--){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str  = "MADAM";
+        System.out.println("Given String : "+str);
+        if(isPalindrome(str)){
+            System.out.println("The give string is palindrome");
+        }else{
+            System.out.println("The given string is not palindrome");
+        }
+    }
+}
