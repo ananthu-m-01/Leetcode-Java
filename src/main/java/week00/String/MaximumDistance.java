aac41815ef12bc31cd5d95924f3a7ca14710211c
Ananthu M <ananthu.m.utr@gmail.com>
2025-04-06 09:30:20 +0530

    Maximum Distance

--- src/main/java/week00/String/MaximumDistance.java
+++ src/main/java/week00/String/MaximumDistance.java
@@ -1,12 +1,14 @@
-package week00.OneDimensionalArray;
+package week00.String;
+
+import java.util.Scanner;
 
 public class MaximumDistance {
     public static int maximumDistance(String str){
         int leftCount = 0, rightCount = 0,leftAnswer = 0,rightAnswer = 0;
         for(int i =0,j = str.length()-1;i<str.length();i++){
             if(str.charAt(i) != str.charAt(j)){
-                 leftAnswer= str.length()-leftCount-1;
-                 break;
+                leftAnswer= str.length()-leftCount-1;
+                break;
             }
             leftCount++;
         }
@@ -20,7 +22,9 @@ public class MaximumDistance {
         return Math.max(leftAnswer,rightAnswer);
     }
     public static void main(String[] args) {
-        String str = "abc10";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the string : ");
+        String str = scanner.nextLine();
         int ans = maximumDistance(str);
         System.out.println("answer : "+ans);
     }
