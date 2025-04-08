93ec0cc7952a86535c4779bbe919f369074296f0
Ananthu M <ananthu.m.utr@gmail.com>
2025-04-08 14:06:23 +0530

    Climb stairs

---
+++ src/main/java/week00/OneDimensionalArray/ClimbStairs.java
@@ -0,0 +1,21 @@
+package week00.OneDimensionalArray;
+
+public class ClimbStairs {
+    public static int climbStairs(int n){
+        int a = 1,b = 1, c = 0;
+        if(n <= 1){
+            return n;
+        }
+        for(int i = 1;i < n;i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        int ans = climbStairs(n);
+        System.out.println("answer : "+ans);
+    }
+}
