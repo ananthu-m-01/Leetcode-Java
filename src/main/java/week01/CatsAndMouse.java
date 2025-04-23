27fc2a67cdbdcf4bf30ff935ad58836bebac83ae
Ananthu M <ananthu.m.utr@gmail.com>
2025-04-23 19:53:32 +0530

    Cat and Mouse

---
+++ src/main/java/week01/CatsAndMouse.java
@@ -0,0 +1,22 @@
+package week01;
+
+public class CatsAndMouse {
+    public static String catAndMouse(int x,int y,int z){
+        int distance1 = Math.abs(x-z);
+        int distance2 = Math.abs(y-z);
+        String winner = "";
+        if(distance1 < distance2){
+            winner = "Cat A";
+        }else if(distance2 < distance1){
+            winner = "Cat B";
+        }else{
+            winner = "Mouse C";
+        }
+        return winner;
+    }
+    public static void main(String[] args) {
+        int x = 2,y = 5,z = 4;
+        String winner = catAndMouse(x,y,z);
+        System.out.println("winner of the game: "+winner);
+    }
+}
